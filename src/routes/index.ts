7a40b04227b8d5a0dede1a6f4e5e1cdb07a76ddb
Ezat <ezatelbery164@gmail.com>
2026-09-25 18:06:40 +0300

    feat(category-library): add category library routes with CRUD operations

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,6 +4,7 @@ import { userRoutes } from "../modules/users/routes/user.routes.js";
 import { authRoutes } from "../modules/auth/routes/auth.route.js";
 import { journalsRoutes } from "../modules/journals/routes/journals.route.js";
 import { moodsRoutes } from "../modules/mood/routes/mood.route.js";
+import { categoryLibraryRoutes } from "../modules/category-library/routes/category-library.route.js";
 
 const router = Router();
 
@@ -14,5 +15,7 @@ router.use("/auth", authRoutes);
 router.use("/moods", moodsRoutes);
 
 router.use("/journals", journalsRoutes);
+
+router.use("category-library", categoryLibraryRoutes);
 
 export { router };
